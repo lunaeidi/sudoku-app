849bc396649c2fa737255b68c222d76fea84f112
Nicole Eidi <nicoleeidi1@gmail.com>
2018-08-27 19:58:34 -0700

    different source for sudoku puzzles, added css

--- src/Components/difficulty.js
+++ src/Components/difficulty.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import './difficulty.css'
 const Difficulty = (props) => {
 return (
+
   <button onClick={(event,level)=>props.difficultySelector(event,props.level)} level={props.level}>{props.level}</button>
+
 )
 }
 
--- src/Containers/difficulty/difficultyContainer.js
+++ src/Containers/difficulty/difficultyContainer.js
@@ -1,5 +1,5 @@
-
-
+import {SudokuGenerator} from 'js-sudoku-generator'
+import './difficultyContainer.css'
 import React, {Component} from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
@@ -12,28 +12,57 @@ class DifficultyContainer extends Component {
         redirected:false
     }
 
-    difficultySelector= (event,level) => {
+    difficultySelector= async (event,level) => {
         console.log(level)
         this.setState({selected:level})
-        console.log('https://sugoku.herokuapp.com/board?difficulty=' + level)
-        fetch('https://sugoku.herokuapp.com/board?difficulty=' + level)
-            .then(res => res.json())
-            .then(json => {
-                this.props.startTimer(new Date())
-                this.props.initBoard(json.board)
-                this.setState({redirected: true})
-            })
+        console.log(SudokuGenerator.generate(1))
+        let generate= await SudokuGenerator.generate(1)
+        let getSheets = await SudokuGenerator.generatedBoards[0];
+        console.log(generate)
+      //  console.log(SudokuGenerator.loadBoard(generate.signature))
+      let loadBoard= await SudokuGenerator.loadBoard(getSheets.signature)
+        let board= null
+
+        if (level=="easy"){board= await getSheets.getSheet(0)}
+        else if (level=="medium"){board= await getSheets.getSheet(1)}
+        else {board= await getSheets.getSheet(2)}
+        //console.log(board)
+        // console.log('https://sugoku.herokuapp.com/board?difficulty=' + level)
+        // fetch('https://sugoku.herokuapp.com/board?difficulty=' + level)
+        //     .then(res => res.json())
+        //     .then(json => {
+        //         this.props.startTimer(new Date())
+        //         this.props.initBoard(json.board)
+        //         this.setState({redirected: true})
+        //     })
+        this.props.startTimer(new Date())
+               board=  await board.map((row)=>{
+          return row.map((square)=>{
+            if (square === ""){
+              return 0
+            } else {
+              return square
+            }
+          })
+        })
+        console.log(board)
+          await this.props.initBoard(board) //pass in the result of it here
+      this.setState({redirected: true})
         };
 
     render(){
         return (
+
                 <div className="difficulty">
+                <div className="sudoku"><h1>SUDOKU</h1></div>
+                <div className="buttons">
                 {this.state.levels.map(level =>
 
                      <Difficulty difficultySelector= {this.difficultySelector} level={level}>{level}</Difficulty>
 
                 )}
             {this.state.redirected ? <Redirect to='/'/> : null}
+            </div>
             </div>
         )
     }
